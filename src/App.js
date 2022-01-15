360bed6393d3beb64999225addabc1e2e78bb24f
armanmoztar <armanmoztar@outlook.com>
2022-01-15 11:44:44 -0800

    update changes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import Container from "react-bootstrap/Container"
 
 function App() {
-    return <Container>
-        <Stack direction="horizontal" gap="2" className="mb-4"/>
+    return (
+    <Container>
+        <Stack direction="horizontal" gap="2" className="mb-4">
         <h1 className="me-auto">Budgets</h1>
         <Button variant="primary">Add Budget</Button>
         <Button variant="outline-primary">Add Expense</Button>
         
         </Stack>
-    </Container>>
+    </Container>
+    )
 }
 
 export default App
